c47922e3af1c300fdc0690ec821d1233670a5aaa
jhigger <njohnjhigger@gmail.com>
2024-10-09 12:50:07 +0800

    fix: color on theme change

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -207,10 +207,10 @@ const ProductList = () => {
             <div className="flex w-full items-baseline justify-between">
               <span>Total</span>
               <span
-                className="mx-2 flex-grow"
+                className="mx-2 flex-grow dark:invert"
                 style={{
                   height: "1px", // Set height for the dashed line
-                  backgroundImage: `url("data:image/svg+xml,%3csvg width=\'100%25\' height=\'100%25\' xmlns=\'http://www.w3.org/2000/svg\'%3e%3ccircle cx=\'1\' cy=\'1\' r=\'1\' fill=\'white\'/%3e%3c/svg%3e")`,
+                  backgroundImage: `url("data:image/svg+xml,%3csvg width=\'100%25\' height=\'100%25\' xmlns=\'http://www.w3.org/2000/svg\'%3e%3ccircle cx=\'1\' cy=\'1\' r=\'1\' fill=\'currentColor\'/%3e%3c/svg%3e")`,
                   backgroundSize: "8px 1px", // Adjust the spacing of the dashes
                   backgroundRepeat: "repeat-x", // Repeat the background image horizontally
                 }}
