4646f4a83d8e14b32ae33cb4911689a214922b29
jhigger <njohnjhigger@gmail.com>
2024-10-16 21:19:36 +0800

    feat: add auto redirect on role switch for /account

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -1,11 +1,13 @@
 import { useUser } from "@clerk/nextjs";
 import { History, KeyRound } from "lucide-react";
 import Head from "next/head";
+import { useRouter } from "next/navigation";
 import MyKeys from "~/components/data-tables/my-key";
 import OrderHistory from "~/components/data-tables/order-history";
 import RootLayout from "~/components/layouts/root-layout";
 import TabsLayout, { type TabType } from "~/components/layouts/tabs-layout";
 import Loader from "~/components/loader";
+import { useUserStore } from "~/state/user.store";
 
 export const ACCOUNT_TABS: TabType[] = [
   {
@@ -23,7 +25,9 @@ export const ACCOUNT_TABS: TabType[] = [
 ] as const;
 
 const Account = () => {
-  const { user, isLoaded } = useUser();
+  const { user: clerkUser, isLoaded } = useUser();
+  const { user } = useUserStore();
+  const router = useRouter();
 
   if (!isLoaded) {
     return (
@@ -33,8 +37,18 @@ const Account = () => {
     );
   }
 
-  if (!user) {
+  if (!clerkUser) {
     return null;
+  }
+
+  // TODO: remove on production
+  if (user?.role === "admin") {
+    router.push("/admin");
+    return (
+      <RootLayout>
+        <Loader />
+      </RootLayout>
+    );
   }
 
   return (
