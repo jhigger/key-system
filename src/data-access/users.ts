5c4945a57536d39496a3bddd84893e90803a74ca
jhigger <njohnjhigger@gmail.com>
2024-10-20 03:44:05 +0800

    fix: switching account supabase error

--- src/data-access/users.ts
+++ src/data-access/users.ts
@@ -45,14 +45,22 @@ export const getUserByClerkId = async (
     return null;
   }
 
+  console.log("Getting user by clerk ID", clerkId);
+
   const { data, error } = await supabase(token)
     .from("users")
     .select("*")
     .eq("clerk_id", clerkId)
-    .single();
+    .maybeSingle();
 
   if (error) {
+    console.log("Error getting user by clerk ID", error);
     throw new Error(`Failed to get user by clerk ID: ${error.message}`);
+  }
+
+  if (!data) {
+    console.log("No user found for clerk ID", clerkId);
+    return null;
   }
 
   const user = {
