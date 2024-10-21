71d9ec1200b59657b6ab718656d29be2e086c85b
jhigger <njohnjhigger@gmail.com>
2024-10-21 21:34:58 +0800

    feat: capitalize

--- src/components/ui/data-table-faceted-filter.tsx
+++ src/components/ui/data-table-faceted-filter.tsx
@@ -128,7 +128,7 @@ export function DataTableFacetedFilter<TData, TValue>({
                     {option.icon && (
                       <option.icon className="mr-2 h-4 w-4 text-muted-foreground" />
                     )}
-                    <span>{option.label}</span>
+                    <span className="capitalize">{option.label}</span>
                     {facets.get(option.value) && (
                       <span className="ml-auto flex h-4 w-4 items-center justify-center font-mono text-xs">
                         {facets.get(option.value)}
