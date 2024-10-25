c4afa1cd0a51819c9f7d4f19fde5641631e9f980
jhigger <njohnjhigger@gmail.com>
2024-10-26 05:18:12 +0800

    fix: login issue when switching account

--- src/hooks/useCurrentUser.ts
+++ src/hooks/useCurrentUser.ts
@@ -8,14 +8,19 @@ import useAuthToken from "./useAuthToken";
 
 export const useCurrentUser = () => {
   const getToken = useAuthToken();
-  const { user: clerkUser, isLoaded: isClerkLoaded } = useUser();
+  const { user: clerkUser, isLoaded: isClerkLoaded, isSignedIn } = useUser();
   const { setUser } = useUserStore();
   const { setEditMode } = useUIStore();
   const [clerkId, setClerkId] = useState("");
-  const { data: user, isLoading } = useQuery({
+  const {
+    data: user,
+    isLoading,
+    isFetched,
+  } = useQuery({
     queryKey: ["user", clerkId],
     queryFn: () => getUserByClerkId(getToken, clerkId),
     enabled: !!clerkId,
+    refetchOnReconnect: "always",
   });
 
   useEffect(() => {
@@ -32,6 +37,12 @@ export const useCurrentUser = () => {
       }
     }
   }, [user, setUser, setEditMode]);
+
+  useEffect(() => {
+    if (isFetched && isSignedIn && !user) {
+      window.location.reload();
+    }
+  }, [isFetched, isSignedIn, user]);
 
   return { user, isLoading: isLoading || !isClerkLoaded };
 };
