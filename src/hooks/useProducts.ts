9c8195e2c5326e6f918d01541ef2ec5c114914fd
jhigger <njohnjhigger@gmail.com>
2024-10-26 02:02:56 +0800

    fix: wrong query key

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -24,7 +24,7 @@ const useProducts = () => {
   });
 
   const pricingsQuery = useQuery({
-    queryKey: ["products"],
+    queryKey: ["pricings"],
     queryFn: () => getPricings(getToken),
   });
 
