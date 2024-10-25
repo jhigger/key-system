b7f4ee7a57a2518cdcb2db4058e5925a0164df4c
jhigger <njohnjhigger@gmail.com>
2024-10-26 04:14:40 +0800

    feat: update order history columns

--- src/components/data-tables/order-history/columns.tsx
+++ src/components/data-tables/order-history/columns.tsx
@@ -1,4 +1,18 @@
 import { type ColumnDef, type Row } from "@tanstack/react-table";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+import { Button } from "~/components/ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "~/components/ui/drawer";
+import { Textarea } from "~/components/ui/textarea";
 import { useCurrentUser } from "~/hooks/useCurrentUser";
 import useMyKeys from "~/hooks/useMyKeys";
 import {
@@ -8,31 +22,6 @@ import {
 } from "~/lib/utils";
 import { type OrderType } from "~/types/order";
 import { DataTableColumnHeader } from "../../data-table-column-header";
-
-const ProductCell: React.FC<{
-  row: Row<OrderType>;
-}> = ({ row }) => {
-  const { user } = useCurrentUser();
-  const { uuid: orderUUID } = row.original;
-  const {
-    productKeySnapshotsByOrderQuery
-  } = useMyKeys(user?.uuid ?? "", orderUUID);
-
-  if (!productKeySnapshotsByOrderQuery.data) return null;
-
-  return (
-    <>
-      {productKeySnapshotsByOrderQuery.data.map((productKey) => (
-        <div
-          key={productKey.uuid}
-          className="-mr-4 -translate-x-4 border-b py-2 pl-4 last:border-b-0"
-        >
-          {productKey.productName}
-        </div>
-      ))}
-    </>
-  );
-};
 
 const VariantCell: React.FC<{
   row: Row<OrderType>;
@@ -46,17 +35,133 @@ const VariantCell: React.FC<{
 
   if (!productKeySnapshotsByOrderQuery.data) return null;
 
+  // Create a Map to track unique product keys and their counts
+  const productKeyCountMap = new Map<string, number>();
+  productKeySnapshotsByOrderQuery.data.forEach((productKey) => {
+    const key = productKey.pricing.uuid;
+    productKeyCountMap.set(key, (productKeyCountMap.get(key) ?? 0) + 1);
+  });
+
   return (
     <>
-      {productKeySnapshotsByOrderQuery.data.map((productKey) => (
+      {Array.from(productKeyCountMap.entries()).map(([key, count]) => (
         <div
-          key={productKey.uuid}
+          key={key}
           className="-mr-4 -translate-x-4 border-b py-2 pl-4 last:border-b-0"
         >
-          {formatDuration(productKey.pricing.duration)}
+          {
+            productKeySnapshotsByOrderQuery.data.find(
+              (pk) => pk.pricing.uuid === key,
+            )?.productName
+          }{" "}
+          ={" "}
+          {formatDuration(
+            productKeySnapshotsByOrderQuery.data.find(
+              (pk) => pk.pricing.uuid === key,
+            )?.pricing.duration
+              ? Number(
+                  productKeySnapshotsByOrderQuery.data.find(
+                    (pk) => pk.pricing.uuid === key,
+                  )?.pricing.duration,
+                )
+              : 0,
+          )}{" "}
+          x {count}
         </div>
       ))}
     </>
+  );
+};
+
+const ActionCell: React.FC<{
+  row: Row<OrderType>;
+}> = ({ row }) => {
+  const { user } = useCurrentUser();
+  const { uuid: orderUUID } = row.original;
+  const { productKeySnapshotsByOrderQuery } = useMyKeys(
+    user?.uuid ?? "",
+    orderUUID,
+  );
+  const [showForm, setShowForm] = useState(false);
+  const [textareaValue, setTextareaValue] = useState("");
+
+  useEffect(() => {
+    if (productKeySnapshotsByOrderQuery.data) {
+      const keysMap: Record<string, string[]> = {};
+      productKeySnapshotsByOrderQuery.data.forEach((pk) => {
+        const productName = pk.productName; // Assuming productName is available in the data
+        const key = pk.key;
+        const duration = formatDuration(pk.pricing.duration); // Assuming pricing.duration is available
+
+        // Create a key for the product and duration
+        const entryKey = `${productName} - ${duration} Keys`;
+        if (!keysMap[entryKey]) {
+          keysMap[entryKey] = [];
+        }
+        keysMap[entryKey].push(key);
+      });
+
+      // Format the textarea value
+      const formattedEntries = Object.entries(keysMap).map(
+        ([entryKey, keys]) => {
+          return `${entryKey}\n${keys.join("\n")}`;
+        },
+      );
+
+      setTextareaValue(formattedEntries.join("\n\n")); // Join all entries with a double newline
+    }
+  }, [productKeySnapshotsByOrderQuery.data]);
+
+  const handleCopy = () => {
+    navigator.clipboard
+      .writeText(textareaValue)
+      .then(() => {
+        toast.success("Keys copied to clipboard");
+      })
+      .catch(() => {
+        toast.error("Failed to copy keys");
+      });
+  };
+
+  if (!productKeySnapshotsByOrderQuery.data) return null;
+
+  return (
+    <Drawer open={showForm} onOpenChange={setShowForm}>
+      <DrawerTrigger asChild>
+        <Button
+          type="button"
+          variant="ghost"
+          className="flex h-9 min-w-20 rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors"
+        >
+          View Keys
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <div className="max-h-[calc(100vh-10rem)] overflow-y-auto">
+          <div className="mx-auto w-full max-w-screen-sm">
+            <DrawerHeader>
+              <DrawerTitle>Purchased Keys</DrawerTitle>
+              <DrawerDescription>
+                You can copy the keys of this order
+              </DrawerDescription>
+            </DrawerHeader>
+            <div className="grid w-full gap-2 px-4">
+              <Textarea
+                value={textareaValue}
+                className="h-[calc(100vh-22rem)]"
+                disabled
+              />
+              <Button onClick={handleCopy}>Copy All</Button>
+            </div>
+            <DrawerFooter>
+              <DrawerClose asChild>
+                <Button variant="outline">Close</Button>
+              </DrawerClose>
+            </DrawerFooter>
+          </div>
+        </div>
+      </DrawerContent>
+    </Drawer>
   );
 };
 
@@ -92,16 +197,9 @@ export const getColumns = (): ColumnDef<OrderType>[] => [
       formatISOStringToDate(row.createdAt).formattedTime,
   },
   {
-    accessorKey: "productKeysSnapshot.productName",
-    header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Product" />
-    ),
-    cell: ({ row }) => <ProductCell row={row} />,
-  },
-  {
     id: "productKeysSnapshot.pricing.duration",
     header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Variant" />
+      <DataTableColumnHeader column={column} title="Product Variant" />
     ),
     cell: ({ row }) => <VariantCell row={row} />,
   },
@@ -109,5 +207,10 @@ export const getColumns = (): ColumnDef<OrderType>[] => [
     accessorKey: "invoiceLink",
     header: "Invoice Link",
     enableSorting: false,
+  },
+  {
+    id: "actions",
+    header: "Actions",
+    cell: ({ row }) => <ActionCell row={row} />,
   },
 ];
