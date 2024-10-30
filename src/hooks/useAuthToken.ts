4785dd1adec488f37136c30438347d9252d526cb
jhigger <njohnjhigger@gmail.com>
2024-10-31 06:24:36 +0800

    chore: remove log

--- src/hooks/useAuthToken.ts
+++ src/hooks/useAuthToken.ts
@@ -7,7 +7,6 @@ const useAuthToken = (): (() => Promise<string | null>) => {
   const fetchToken = useCallback(async (): Promise<string | null> => {
     try {
       const token = await getToken({ template: "supabase" });
-      console.log(token ? "Token exists" : "No token");
       return token;
     } catch (error) {
       console.error("Error retrieving token:", error);
